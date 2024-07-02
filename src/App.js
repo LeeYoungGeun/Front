25ea344a6eceece9f3c3245e459c566b920d0401
YoungGeun <crusade6702@gmail.com>
2024-07-02 15:27:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [hello, setHello] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8090/api/test')
+        .then((res) => {
+          console.log(res.data);
+          setHello(res.data);
+        })
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        백엔드 데이터 : {hello}
+      </div>
   );
 }
 
