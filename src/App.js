a43251f78c9ba2fbe1677848e6cabdc5d0f54dc4
[LYG] <crusade6702@gmail.com>
2024-07-03 18:32:51 +0900

    main page 1 commit by lyg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
-
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  MainContainer, 
+  MainHeader, 
+  MainHeaderLogoArea, MainHeaderSearchArea, MainHeaderButtonArea,
+  MainBody,
+  MainBodyRollingBannerArea,
+  MainBodyMovieListArea,
+  MainBodyMovieListSection,
+  MainFooter
+} from "./styled-components/Main";
+
 
 function App() {
-
-  const [hello, setHello] = useState('');
-
-  useEffect(() => {
-    axios.get('http://localhost:8090/api/test')
-        .then((res) => {
-          console.log(res.data);
-          setHello(res.data);
-        })
-  }, []);
   return (
-      <div className="App">
-        백엔드 데이터 : {hello}
-      </div>
+    <MainContainer color='black'>
+      <MainHeader>
+        <MainHeaderLogoArea>logo</MainHeaderLogoArea>
+        <MainHeaderSearchArea>search</MainHeaderSearchArea>
+        <MainHeaderButtonArea>button</MainHeaderButtonArea>
+      </MainHeader>
+
+      <MainBody>
+        <MainBodyRollingBannerArea>
+          banner
+        </MainBodyRollingBannerArea>
+        <MainBodyMovieListArea>
+          <MainBodyMovieListSection>movie list 1</MainBodyMovieListSection>
+          <MainBodyMovieListSection>movie list 2</MainBodyMovieListSection>
+          <MainBodyMovieListSection>movie list 3</MainBodyMovieListSection>
+          <MainBodyMovieListSection>movie list 4</MainBodyMovieListSection>
+          <MainBodyMovieListSection>movie list 5</MainBodyMovieListSection>
+        </MainBodyMovieListArea>
+      </MainBody>
+      <MainFooter>
+        ⓒ 2024. TFT all rights reserved.
+      </MainFooter>
+    </MainContainer>
   );
 }
 
