c5d2548c9e3c053e16baac201ea485861d658f7e
Mingyeol <alsruf741@gmail.com>
2024-07-03 12:52:10 +0900

    로그인 회원가입UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import {useEffect, useState} from "react";
 import './App.css';
-import Test from "./Test";
-import SignUp from "./Member/SignUp";
-import Login from "./Member/Login";
 import { createGlobalStyle } from "styled-components";
+import { Link, Route,Routes, useLocation } from "react-router-dom";
+import HomeTest from './HomeTest';
+import SignUp from './Member/SignUp';
+import Login from './Member/Login';
 const GlobalStyle = createGlobalStyle`
   body {
     background-image: url('https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg');
@@ -12,13 +12,19 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+
 function App() {
 
  return(
-    <div>
-    <GlobalStyle/>
-    <Login/>
-    </div>
+    <>
+     <GlobalStyle/> 
+      
+      <Routes>
+        <Route path='/' element={HomeTest} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </>
  )
 }
 
--- src/Member/SignUp.js
+++ src/Member/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import api from "./apiTest";
+import "./SignUp.css";
 
 
 function SignUp(){
@@ -30,18 +31,20 @@ function SignUp(){
 
     return(
         <div>
-    <h2>회원가입</h2>
-      <form onSubmit={handleSignUp}>
-        <input name="mid" value={signUpData.mid} onChange={handleSignUpChange} placeholder="ID" />
-        <input name="mpw" type="password" value={signUpData.mpw} onChange={handleSignUpChange} placeholder="Password" />
-        <input name="checkMpw" type="password" value={signUpData.checkMpw} onChange={handleSignUpChange} placeholder="PasswordCheck" />
-        <input name="mname" value={signUpData.mname} onChange={handleSignUpChange} placeholder="Name" />
-        <input name="mnick" value={signUpData.mnick} onChange={handleSignUpChange} placeholder="Nickname" />
-        <input name="memail" type="email" value={signUpData.memail} onChange={handleSignUpChange} placeholder="Email" />
-        <input name="mphone" value={signUpData.mphone} onChange={handleSignUpChange} placeholder="Phone" />
-        <button type="submit">회원가입</button>
-      </form>
+        <div className="signup-container">
+          <form className="signup-form" onSubmit={handleSignUp}>
+            <h2>회원가입</h2>
+            <input name="mname" value={signUpData.mname} onChange={handleSignUpChange} placeholder="Name" />
+            <input name="mid" value={signUpData.mid} onChange={handleSignUpChange} placeholder="ID" />
+            <input name="mpw" type="password" value={signUpData.mpw} onChange={handleSignUpChange} placeholder="Password" />
+            <input name="checkMpw" type="password" value={signUpData.checkMpw} onChange={handleSignUpChange} placeholder="Password Check" />
+            <input name="mnick" value={signUpData.mnick} onChange={handleSignUpChange} placeholder="Nickname" />
+            <input name="memail" type="email" value={signUpData.memail} onChange={handleSignUpChange} placeholder="Email" />
+            <input name="mphone" value={signUpData.mphone} onChange={handleSignUpChange} placeholder="Phone" />
+            <button type="submit">회원가입</button>
+          </form>
         </div>
+      </div>
     )
 }
 
