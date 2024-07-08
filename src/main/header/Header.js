f5f8a08d7577cdeb063046d04580089d6e681b6d
Mingyeol <alsruf741@gmail.com>
2024-07-08 12:11:37 +0900

    로그인 후 로그아웃 버튼 활성

--- src/main/header/Header.js
+++ src/main/header/Header.js
@@ -1,26 +1,60 @@
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+
 import {
-    MainHeader, 
-    MainHeaderLogoArea, MainHeaderSearchArea, MainHeaderButtonArea,
-    SearchBar, Button
-  } from '../Main';
+  MainHeader, 
+  MainHeaderLogoArea, MainHeaderSearchArea, MainHeaderButtonArea,
+  SearchBar,
+  Button
+} from '../Main';
 
 function Header() {
+  const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (cookies.accessToken) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [cookies.accessToken]);
+
+  const handleLogout = () => {
+    removeCookie('accessToken', { path: '/' });
+    removeCookie('refreshToken', { path: '/' });
+    setIsAuthenticated(false);
+    navigate('/');
+    alert("로그아웃 되었습니다.");
+  };
 
   return (
     <MainHeader>
-       <MainHeaderLogoArea><Link to="/">TFT</Link></MainHeaderLogoArea>
-        <MainHeaderSearchArea>
-        <SearchBar></SearchBar> 
-        </MainHeaderSearchArea>
-        <MainHeaderButtonArea>
-        <Link to="/login"><Button>로그인</Button></Link>
+      <MainHeaderLogoArea>
+        <Link to="/">TFT</Link>
+      </MainHeaderLogoArea>
+      <MainHeaderSearchArea>
+        <SearchBar></SearchBar>
+      </MainHeaderSearchArea>
+      <MainHeaderButtonArea>
+        {isAuthenticated ? (
+          <>
+            <Button onClick={handleLogout}>로그아웃</Button>
+          </>
+        ) : (
+          <>
+            <Link to="/login"><Button>로그인</Button></Link>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to="/signup"><Button>회원가입</Button></Link>
+          </>
+        )}
         &nbsp;&nbsp;&nbsp;&nbsp;
-        <Link to="/signup"><Button>게시판</Button></Link>
-        </MainHeaderButtonArea>
+        <Link to="/#"><Button>게시판</Button></Link>
+      </MainHeaderButtonArea>
     </MainHeader>
   );
-
 }
 
 export default Header;
