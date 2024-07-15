574733d6156d7d8e152fc050af83252343df2e0f
[LYG] <crusade6702@gmail.com>
2024-07-15 18:34:41 +0900

    db working by lyg

--- src/modal/useReviews.js
+++ src/modal/useReviews.js
@@ -21,29 +21,67 @@ const useReviews = (movie_id,movie_title) => {
       setReview('');
       setRating(0);
       
-      let option = {
-        method: 'post',
-        url: 'http://localhost:8090/movie/register',
-        headers: { 
+      const headers_val = {
           'Content-Type': 'application/json', 
           'Authorization': `Bearer ${getCookie()}`
-        },
+      }
+
+      let option_movie_register = {
+        method: 'post',
+        url: 'http://localhost:8090/api/movie/register',
+        headers: headers_val,
         data : JSON.stringify({
           "movie_id": movie_id,
           "movie_title": movie_title
         })
       };
 
-      axios.request(option)
+      let option_review_register = {
+        method: 'post',
+        url: 'http://localhost:8090/api/review/register',
+        headers: headers_val,
+        // movie_id, user_id , review_text, review_star
+        data : JSON.stringify({
+          "movie_id": movie_id,
+          "review_text": review,
+          "review_star": rating
+        })
+      };
+
+      let option_review_list = {
+        method: 'post',
+        url: 'http://localhost:8090/api/review/listOfReveiw',
+        headers: headers_val,
+        // movie_id, user_id , review_text, review_star
+        data : JSON.stringify({
+          "movie_id": movie_id
+        })
+      };
+
+      // useEffect(() => {
+
+      // })
+
+      axios.request(option_movie_register)
+      .then((response) => {
+        //해당 영화 id가 db id와 일치하면
+        if (response.data.result === movie_id) { 
+          //댓글 등록 api 호출
+          return axios.request(option_review_register)
+        }else{
+        //일치 하지않으면
+          alert("영화 정보가 일치하지 않습니다.")
+          return false;
+        }
+      })
       .then((response) => {
         console.log(JSON.stringify(response.data));
+
       })
       .catch((error) => {
         console.log(error);
       });
-
     }
-
   };
 
   return { rating, setRating, review, setReview, reviews, handleSubmitReview };
